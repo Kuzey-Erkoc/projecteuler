b66649f46d5049edbff6ae9ff558eae40162a0a6
Kuzey-Erkoc <88731738+Kuzey-Erkoc@users.noreply.github.com>
2025-01-05 13:23:47 +0300

    Update euler5.py

--- euler5.py
+++ euler5.py
@@ -1,15 +1,15 @@
 #5
-def gcd(a, b):
+def ebob(a, b):
     while b:
         a, b = b, a % b
     return a
 
-def lcm(a, b):
-    return abs(a * b) // gcd (a, b)
+def ekok(a, b):
+    return abs(a * b) // ebob (a, b)
 
 result = 1
 
 for i in range(1, 21):
-    result = lcm(result, i)
+    result = ekok(result, i)
 
 print(result)
